9c5382e762ad00cee2891e2a557f67e1d739e0df
Maksym Havrylov <salomondr007@gmail.com>
2021-09-23 22:57:30 +0300

    add smoothScroll.js

---
+++ js/smoothScroll.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const linksHead = document.querySelectorAll('.menu-list__link');
+
+// linksHead.forEach(link => {
+//   link.addEventListener('click', (event) => {
+
+//   })
+// })
