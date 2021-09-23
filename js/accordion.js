6ee2b66a77a0d06b1c724a11d34732df648aa87b
Maksym Havrylov <salomondr007@gmail.com>
2021-09-23 22:53:55 +0300

    accordion toggle and create smoothScroll.js

--- js/accordion.js
+++ js/accordion.js
@@ -1,28 +1,18 @@
-// 'use strict';
+'use strict';
 
 const lists = document.querySelectorAll('.feature-sub');
 const btns = document.querySelectorAll('.feature__link');
 
-// btn.addEventListener('click', () => {
-
-// });
-
-// for (let i = 0; i < btns.length; i++) {
-//   console.log(btns[i]);
-// };
-
-btns.forEach((btnItem, index) => {
-  btnItem.addEventListener('click', () => {
-    btns.forEach((btnItem) => {
-      btnItem.classList.remove('feature__link_active');
-    });
-
-    btnItem.classList.add('feature__link_active');
-
-    lists.forEach((listItem) => {
-      listItem.classList.add('hidden');
-    });
-
-    lists[index].classList.remove('hidden')
+btns.forEach((btn, index) => {
+  btn.addEventListener('click', () => {
+    btns.forEach((btnItem, idx) => {
+      if (btnItem === btn) {
+        btnItem.classList.toggle('feature__link_active')
+        lists[idx].classList.toggle('hidden')
+      } else {
+        btnItem.classList.remove('feature__link_active')
+        lists[idx].classList.add('hidden')
+      }
+    })
   })
 });
