c4202dc283fc6ea0791fc9f0657b6ac1826cc08c
Maksym Havrylov <salomondr007@gmail.com>
2021-09-25 21:46:53 +0300

    done smoothScroll.js and create tabs.js

--- js/smoothScroll.js
+++ js/smoothScroll.js
@@ -1,9 +1,18 @@
 'use strict';
 
 const linksHead = document.querySelectorAll('.menu-list__link');
+const mainScroll = document.querySelector('.main__scroll');
+const newArray = [...linksHead, mainScroll];
 
-// linksHead.forEach(link => {
-//   link.addEventListener('click', (event) => {
+newArray.forEach(link => {
+  link.addEventListener('click', (event) => {
+    event.preventDefault()
 
-//   })
-// })
+    const ID = event.target.getAttribute('href').substr(1)
+
+    document.getElementById(ID).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    })
+  })
+})
